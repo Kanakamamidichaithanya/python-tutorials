3446da74d44c180f46caf4b2ad8992f1cb2aa487
kanakamamidi chaithanya <kanakamamidichaithanya@gmail.com>
2024-10-01 09:25:39 +0530

    basic programs in functions.py

---
+++ functions.py
@@ -0,0 +1,59 @@
+
+# def sum(a,b):
+#     sum = a+b
+#     return sum
+# a = int(input("enter first number"))              sum of two numbers using fuctiion calling
+# b = int(input("enter the second number"))
+# print(sum(a,b))
+
+
+# def avg(a,b,c):
+#     avg = (a+b+c)/2                                finding average using fuction call
+#     return avg
+# print(avg(1,2,3))
+
+
+
+# list = ("a","b","d","d")
+# count = len(list)
+# print(count)
+  
+
+# cities = ("hyderabd","mumbai", "nagpur", "banglore","manglore")
+# heroes = ["aa","mb","prabhas","sk"]
+# def set(list):
+#     print(len(list))
+
+# def list(list):
+#     print(len(list))
+
+# print(set(cities))    
+# print(len(heroes))    
+
+
+
+# cities = ["hyderabd","mumbai", "nagpur", "banglore","manglore"]
+# heroes = ["aa","mb","prabhas","sk"]
+# def count(list):
+#     print(len(list))
+# count(cities)  
+# count(heroes)
+
+
+
+# def calc_fact(n):
+#     fact =1
+#     for i in range(1,n+1):                  finding factorail using fuction calling
+#         fact = fact * i
+#     print(fact)
+# calc_fact(5)
+
+
+
+# def conv_ind(amount):
+#     while amount>0:
+#         converted_amount = amount* 70
+#         return converted_amount
+# result = conv_ind(20)  
+# print(result)
+
