5b46891eae498a999bea640b78e172c2664fba49
kanakamamidi chaithanya <kanakamamidichaithanya>
2026-08-20 22:28:10 +0530

    added some code into the armstrong.py file

--- armstrong.py
+++ armstrong.py
@@ -1,15 +1,15 @@
-# a = 153
-# temp = a 
-# arm = 0
-# n = len(str(a))
-# while temp > 0:
-#     rem = temp % 10
-#     arm = arm + (rem**n)
-#     temp = temp//10
-# if  a == arm:
-#     print("is arm")
-# else :
-#     print("not not arm")          
+a = 153
+temp = a 
+arm = 0
+n = len(str(a))
+while temp > 0:
+    rem = temp % 10
+    arm = arm + (rem**n)
+    temp = temp//10
+if  a == arm:
+    print("is arm")
+else :
+    print("not not arm")          
 
 
 # def isarmstrong(number):
@@ -26,3 +26,39 @@
 
 
 
+# def armstrong(number):
+#     temp = number
+#     arm = 0
+#     n = len(str(number))
+#     while temp>0:
+#         rem = temp%10
+#         arm = arm + (rem**n)
+#         temp = temp // 10
+
+#     if arm == number:
+#         print(number, "is an Armstrong Number")
+#     else:
+#         print(number, "is NOT an Armstrong Number")
+
+# num = 121
+# armstrong(num)
+
+
+# def armstrong(number):
+#     temp = number
+#     n = len(str(number))
+#     arm = 0
+
+#     while temp > 0:
+#         rem = temp % 10
+#         arm = arm + (rem ** n)
+#         temp = temp // 10
+
+#     if arm == number:
+#         print(number, "is an Armstrong Number")
+#     else:
+#         print(number, "is NOT an Armstrong Number")
+
+
+# num = int(input("Enter a number: "))
+# armstrong(num)
